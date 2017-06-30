272247f363526d082c83c0db04b6c7fd95d64876
James Reggio <james.reggio@gmail.com>
2017-06-30 11:09:59 -0400

    Don't attempt non-configurable properties

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var objectPrototype = getPrototypeOf && getPrototypeOf(Object);
 function copyProperty(targetComponent, sourceComponent, key) {
     try { // Avoid failures from read-only properties
         var descriptor = getOwnPropertyDescriptor(sourceComponent, key);
-        if (descriptor.writable !== false) {
+        if (descriptor.writable !== false && descriptor.configurable !== false) {
             defineProperty(targetComponent, key, descriptor);
         }
     } catch (e) {}
